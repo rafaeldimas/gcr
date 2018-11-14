3255426a8fb6d5abef4a5157cea682c306016ee9
Rafael Dimas <rafael.dimas@live.com>
2018-11-14 20:51:34 -0200

    Upgrade Plugins Akismet and ContactForm7, and upgrade Languages Wordpress, change Title of Curriculumns for Coordenação, added Sliders of Home page

--- wp-content/themes/gcr/template-parts/curriculums/list-about.php
+++ wp-content/themes/gcr/template-parts/curriculums/list-about.php
@@ -1,7 +1,7 @@
 <?php $curriculums = getCurriculums() ?>
 <?php if ($curriculums->have_posts()) : ?>
 <div class="curriculumns has-padding-bt-1 has-padding-lr-6">
-    <h2 class="has-text-primary has-text-centered is-size-3">Equipe</h2>
+    <h2 class="has-text-primary has-text-centered is-size-3">Coordenação</h2>
     <div class="columns">
         <?php foreach ($curriculums->posts as $curriculum) : ?>
             <?php get_template_part_with_attr('template-parts/curriculums/content', 'about', compact('curriculum')) ?>
