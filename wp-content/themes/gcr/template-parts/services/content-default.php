54fec455268068b7d8a3d116467c26878a6c5166
Rafael Dimas <rafael.dimas@live.com>
2018-11-20 00:11:01 -0200

    added support docker, added images in services

--- wp-content/themes/gcr/template-parts/services/content-default.php
+++ wp-content/themes/gcr/template-parts/services/content-default.php
@@ -9,13 +9,13 @@ $leftOrRight = ($key % 2 === 0) ? 'left' : 'right';
 $backgroundAling = $hasThumbnail ? "bg-aling-{$leftOrRight}" : '';
 $textAling = "has-text-{$leftOrRight} has-block-{$leftOrRight}";
 
-$backgroundClass = $hasThumbnail ? "has-margin-bt-1 has-padding-bt-1 bg-service {$backgroundAling}" : '';
+$backgroundClass = $hasThumbnail ? "has-margin-bt-6 bg-service {$backgroundAling}" : '';
 ?>
 
 <div class="<?php echo $backgroundClass ?> column is-12 has-padding-lr-6" <?php echo $style ?>>
     <div class="service <?php echo $textAling ?>">
-        <h2 class="title has-text-primary"><?php echo $post->post_title ?></h2>
-        <div class="content has-text-primary has-text-justified">
+        <h2 class="title has-text-secundary"><?php echo $post->post_title ?></h2>
+        <div class="content has-text-secundary has-text-weight-bold	has-text-justified">
             <?php echo apply_filters('the_content', $post->post_content) ?>
         </div>
     </div>
