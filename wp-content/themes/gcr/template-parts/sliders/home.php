16d29195f5a60f205c7e878fa458da47a919f7d0
Rafael Dimas <rafael.dimas@live.com>
2018-12-01 14:21:00 -0200

    Change image slider 3 home, and added box in content sliders home and added custom fields for aling box content

--- wp-content/themes/gcr/template-parts/sliders/home.php
+++ wp-content/themes/gcr/template-parts/sliders/home.php
@@ -6,8 +6,12 @@
             <?php foreach ($sliders->posts as $post) : ?>
                 <?php $postThumbnailUrl = get_the_post_thumbnail_url($post->ID, 'slider-fullwidth') ?>
                 <?php $style = $postThumbnailUrl ? "style='background-image: url({$postThumbnailUrl})'" : '' ?>
+                <?php $positionX = get_post_meta($post->ID, 'position_x', true) ?>
+                <?php $positionY = get_post_meta($post->ID, 'position_y', true) ?>
+                <?php $classPositionX = $positionX ? "position-x-{$positionX}" : 'position-x-center' ?>
+                <?php $classPositionY = $positionY ? "position-y-{$positionY}" : 'position-y-center' ?>
                 <li class="item js_slide" <?php echo $style ?>>
-                    <div class="content">
+                    <div class="content <?php echo "{$classPositionX} {$classPositionY}"  ?>">
                         <?php echo apply_filters('the_content', $post->post_content) ?>
                     </div>
                 </li>
