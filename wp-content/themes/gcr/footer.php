5e8af51eabef91df43bab8cca18192c53a973702
Rafael Dimas <rafael.dimas@live.com>
2019-02-07 22:03:46 -0200

    Added Google Analytics

--- wp-content/themes/gcr/footer.php
+++ wp-content/themes/gcr/footer.php
@@ -13,6 +13,14 @@
 </footer>
 
 <?php wp_footer(); ?>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-134130772-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'UA-134130772-1');
+</script>
 </body>
 </html>
